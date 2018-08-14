b43c3c708d813d19c3ddd778ca06836e1d50b087
Marco Hyyryläinen <marco@wheresmar.co>
2018-08-14 12:13:13 +0200

    Added some readme and code (not tested, it will break)

--- readme.md
+++ readme.md
@@ -1,11 +1,69 @@
+
+
 # Logga
-Simple logger for PHP stuff. Add as a composer package and go nuts.
+
+Simple logger for PHP stuff. Supports logging to PHP error_log, sending emails, and calling webhooks. Add as a composer package and go nuts.
+
+Based on the [VIP Go MU Plugins Logger](https://github.com/Automattic/vip-go-mu-plugins/blob/master/000-debug/logger.php).
 
 ## Requirements
-TODO
 
-## Getting started
-TODO
+PHP 5.4.0 or later.
+
+## Composer
+
+You can install the bindings via [Composer](http://getcomposer.org/). Run the following command:
+
+```bash
+composer require wm/logga
+```
+
+To use the bindings, use Composer's [autoload](https://getcomposer.org/doc/01-basic-usage.md#autoloading):
+
+```php
+require_once('vendor/autoload.php');
+```
+
+## Getting Started
+
+Simple usage looks like:
+
+```php
+$l = new \WM\Logga();
+$l->text('A thing to log');
+```
 
 ## Usage
-TODO
+
+### Text (to PHP error_log)
+
+ * `$l->text($something_to_log);` - error_log(print_r($something_to_log, true));
+ * `$l->text(compact('v1','v2');` - log several variables with labels
+ * `$l->text($thing5, $thing10);` - log two things
+ * `$l->text();`  - log the file:line
+ * `$l->text(null, $stuff, $ba);` - log the file:line, then log two things.
+
+The first call of `$l->text()` will print an extra line containing a random ID & PID and the script name or URL. The ID prefixes every `$l->text()` log entry thereafter. The extra line and ID will help you to indentify and correlate log entries.
+
+ **Example:**
+`$l->text('yo')`
+`$l->text('dude')`
+
+ **/tmp/php-errors:**
+```
+ * 	[21-Jun-2012 14:45:13] 1566-32201 => /home/wpcom/public_html/bin/wpshell/wpshell.php
+ * 	[21-Jun-2012 14:45:13] 1566-32201 yo
+ * 	[21-Jun-2012 14:50:23] 1566-32201 dude
+```
+`$l->text()` returns its input so you can safely wrap most kinds of expressions to log them.
+`$l->text($arg1, $arg2)` will call `$l->text($arg1)` and `$l->text($arg2)` and then return `$arg1`.
+
+A null argument will log the file and line number of the `$l->text()` call.
+
+### Email
+
+To be added.
+
+### Webhook
+
+To be added.
--- src/Logga.php
+++ src/Logga.php
@@ -5,8 +5,8 @@
  * Simple logger for PHP stuff. Add as a composer package and go nuts.
  * Includes support to log to error_log, via email, and webhook.
  *
- * @package    Logga
- * @author     Marco Hyyryläinen <marco@wheresmar.co>
+ * @package Logga
+ * @author  Marco Hyyryläinen <marco@wheresmar.co>
  */
 
 namespace WM;
@@ -17,9 +17,101 @@ class Logga {
 	 * Log an messages to PHP error_log
 	 *
 	 * @since    1.0.0
+	 *
+	 * @param anything $stuff The thing you want to log.
 	 */
-	public function txt() {
-		throw new \Exception( "Not implemented" );
+	public function text( $stuff = null ) {
+		static $pageload;
+
+		// Call $this->text() on each argument.
+		if ( func_num_args() > 1 ) :
+			foreach ( func_get_args() as $arg ) :
+				$this->text( $arg );
+			endforeach;
+
+			return $stuff;
+		endif;
+
+		if ( ! isset( $pageload ) ) :
+
+			$pageload = substr( md5( mt_rand() ), 0, 4 );
+
+			if ( ! empty( $_SERVER['argv'] ) ) :
+				$hint = implode( ' ', $_SERVER['argv'] );
+			elseif ( isset( $_SERVER['HTTP_HOST'] ) && isset( $_SERVER['REQUEST_URI'] ) ) :
+				$hint = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+			else :
+				$hint = php_sapi_name();
+				error_log( sprintf( '[%s-%s => %s]', $pageload, getmypid(), $hint ) );
+			endif;
+
+		endif;
+
+		$pid = $pageload . '-' . getmypid();
+
+		if ( is_null( $stuff ) ) :
+
+			// Log the file and line number.
+			$backtrace = debug_backtrace( false );
+			while ( isset( $backtrace[1]['function'] ) && __FUNCTION__ === $backtrace[1]['function'] ) :
+				array_shift( $backtrace );
+			endwhile;
+
+			$log = sprintf( '%s line %d', $backtrace[0]['file'], $backtrace[0]['line'] );
+
+		elseif ( is_bool( $stuff ) ) :
+			$log = $stuff ? 'TRUE' : 'FALSE';
+		elseif ( is_scalar( $stuff ) ) :
+			$log = $stuff; // Strings and numbers can be logged exactly.
+		else :
+			// Are we in an output buffer handler?
+			// If so, print_r($stuff, true) is fatal so we must avoid that.
+			// This is not as slow as it looks: <1ms when !$in_ob_handler.
+			// Using json_encode_pretty() all the time is much slower.
+			do {
+				$in_ob_handler = false;
+				$ob_status = ob_get_status( true );
+				if ( ! $ob_status ) :
+					break;
+				endif;
+
+				foreach ( $ob_status as $ob ) :
+					$obs[] = $ob['name'];
+				endforeach;
+
+				// This is not perfect: anonymous handlers appear as default.
+				if ( array( 'default output handler' ) === $obs ) :
+					break;
+				endif;
+
+				$backtrace = debug_backtrace( false );
+
+				foreach ( $backtrace as $level ) :
+					$caller = '';
+
+					if ( isset( $level['class'] ) ) :
+						$caller = $level['class'] . '::';
+					endif;
+
+					$caller .= $level['function'];
+					$bts[]   = $caller;
+				endforeach;
+
+				if ( array_intersect( $obs, $bts ) ) :
+					$in_ob_handler = true;
+				endif;
+			} while ( false );
+
+			if ( $in_ob_handler ) :
+				$log = l_json_encode_pretty( $stuff );
+			else :
+				$log = print_r( $stuff, true );
+			endif;
+		endif;
+
+		error_log( sprintf( '[%s] %s', $pid, $log ) );
+
+		return $stuff;
 	}
 
 	/**
@@ -28,7 +120,7 @@ class Logga {
 	 * @since    1.0.0
 	 */
 	public function mail() {
-		throw new \Exception( "Not implemented" );
+		return 'test';
 	}
 
 	/**
@@ -37,7 +129,7 @@ class Logga {
 	 * @since    1.0.0
 	 */
 	public function hook() {
-		throw new \Exception( "Not implemented" );
+		return 'test';
 	}
 
 }
